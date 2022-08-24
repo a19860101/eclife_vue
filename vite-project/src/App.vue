82ccd12f2aa33573508b0973d5c7853e2e745ea2
Zac <a19860101@gmail.com>
2022-08-24 20:52:59 +0800

    20220824

---
+++ vite-project/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+  import Nav from './components/Nav.vue'
+  
+  export default {
+    components:{
+      Nav
+    }
+  }
+</script>
+<template>
+  <Nav></Nav>
+  <h1>hello vite</h1>
+
+</template>
+<style>
+  h1 {
+    color: red;
+  }
+</style>
---
+++ vite-project/src/components/Nav.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>nav</h1>
+    <nav>
+        <a href="#">link</a>
+        <a href="#">link</a>
+        <a href="#">link</a>
+        <a href="#">link</a>
+        <a href="#">link</a>
+    </nav>
+</template>
+<style scoped>
+    h1 {
+        color: green;
+    }
+</style>
